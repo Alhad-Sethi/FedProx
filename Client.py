bb947750921be766d36208d1bb45f21a6172a9eb
Alhad Sethi <alhad.sethi03@gmail.com>
2024-12-17 01:49:10 +0530

    Client class

---
+++ Client.py
@@ -0,0 +1,38 @@
+
+
+class Client:
+    def __init__(self, model, dataloader, lr=0.01, local_epochs=5, mu=1e4):
+
+        self.model = model
+        self.dataloader = dataloader
+        self.lr = lr
+        self.local_epochs = local_epochs
+        self.mu = mu  # fedprox regularization term
+
+    def synchronize(self, global_state_dict):
+        self.model.load_state_dict(global_state_dict)
+
+    def train(self, global_state_dict):
+
+        self.model.train()
+        optimizer = optim.SGD(self.model.parameters(), lr=self.lr)
+        loss_fn = nn.CrossEntropyLoss()
+
+        global_params = {name: param.clone().detach() for name, param in global_state_dict.items()}
+
+        for epoch in range(self.local_epochs):  
+            for X, y in self.dataloader:
+                optimizer.zero_grad()
+                outputs = self.model(X)
+
+                loss = loss_fn(outputs, y)
+
+                proximal_term = 0.0
+                for name, param in self.model.named_parameters():
+                    proximal_term += torch.sum((param - global_params[name]) ** 2)
+                loss += (self.mu / 2) * proximal_term  # fedbrox loss
+
+                loss.backward()
+                optimizer.step()
+
+        return self.model.state_dict()
